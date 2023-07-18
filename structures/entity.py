16eb21365ab2610aa08add49fdc0345598255552
ModernTy <antonaw2004@gmail.com>
2023-07-18 11:02:52 +0300

    Added Inventory class

--- structures/entity.py
+++ structures/entity.py
@@ -1,3 +1,24 @@
+from .general import TagClass
+from .item import Item
+
+class Inventory(TagClass):
+    do_camel_case = False
+    
+    def __init__(self,
+                 mainhand: Item,
+                 offhand: Item,
+                 head: Item,
+                 chest: Item,
+                 legs: Item,
+                 boots: Item
+                 ) -> None:
+        self.mainhand = mainhand
+        self.offhand = offhand
+        self.head = head
+        self.chest = chest
+        self.legs = legs
+        self.boots = boots
+    
 
 class EntityNBT:
     def __init__(self) -> None:
